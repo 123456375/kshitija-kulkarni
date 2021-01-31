a8054eedbb11e623749e0c65025bbd17789417ea
7721978940 <78317775+7721978940@users.noreply.github.com>
2021-02-01 00:28:05 +0530

    Add files via upload

---
+++ Nseries.java
@@ -0,0 +1,25 @@
+package Assignment;
+
+public class Nseries {
+
+	public static void main (String args [])
+	{
+	Scanner in =new Scanner (System.in);
+	int p=2,i,s=165,n;
+	System.out.println ("Enter the value of n");
+	n=in.nextInt ();
+	for (i=1;i <=n;i++)
+	{
+	if(p%2==0)
+	{
+	s=s+30;
+	}
+	else
+	{
+	s=s+60;
+	}
+	System.out.println (s+",");
+	p++;
+	}
+	}
+	}
